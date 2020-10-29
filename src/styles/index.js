6135ffa2b712399a61938d923ee4e3c02ef6af99
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2020-10-29 18:40:58 -0300

    Add personal font sizes

---
+++ src/styles/index.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const H1 = styled.h1`
+  font-size: 60px;
+  line-height: 76px;
+`;
+
+export const H2 = styled.h2`
+  font-size: 30px;
+  line-height: 30px;
+`;
+
+export const H3 = styled.h3`
+  font-size: 18;
+  line-height: 23px;
+`;
+
+export const Text = styled.p`
+  margin-bottom: 16px;
+  line-height: 20px;
+`;
