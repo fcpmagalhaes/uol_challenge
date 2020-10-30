df6626158dcc51cfbbfd89261c23d68b9083cd53
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2020-10-30 02:55:55 -0300

    Create logged page

---
+++ src/pages/logged/Logged.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import {Container, Row, Col } from 'styled-bootstrap-grid';
+import ContentWrapper from './styles';
+import { H1, H2, H3, Text } from '../../styles';
+
+export default function Logged() {
+  return(
+    <ContentWrapper>
+      <H1>Logged</H1>
+    </ContentWrapper>
+  )
+}
--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,14 @@ import {
 } from 'react-router-dom';
 
 import Login from './pages/login';
+import Logged from './pages/logged';
 
 function Routes() {
   return (
     <Router>
       <Switch>
         <Route path="/" exact component={Login} />
+        <Route path="/logged" component={Logged} />
       </Switch>
     </Router>
   );
