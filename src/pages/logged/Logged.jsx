7ad37038c921ad54d3130345f9c93959b80af983
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2020-10-31 20:46:31 -0300

    Add transition between pages

--- src/pages/logged/Logged.jsx
+++ src/pages/logged/Logged.jsx
@@ -12,7 +12,7 @@ import iconCloud from '../../assets/logged/icon_cloud.svg';
 export default function Logged() {
   const history = useHistory();
   const [countDown, setcountDown] = useState(60);
-  const [time, setTime] = useState();
+  const [time, setTime] = useState("- : -");
   const [date, setDate] = useState();
   
   function logout() {
--- src/routes.js
+++ src/routes.js
@@ -2,17 +2,27 @@ import React from 'react';
 import {
   BrowserRouter as Router, Route, Switch,
 } from 'react-router-dom';
-
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import Login from './pages/login';
 import Logged from './pages/logged';
 
 function Routes() {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact component={Login} />
-        <Route path="/logged" component={Logged} />
-      </Switch>
+      <Route render={({ location }) => (
+        <TransitionGroup>
+          <CSSTransition
+            key={location.key}
+            timeout={300}
+            classNames='fade'
+          >
+            <Switch location={ location }>
+              <Route path="/" exact component={Login} />
+              <Route path="/logged" component={Logged} />
+            </Switch>
+          </CSSTransition>
+        </TransitionGroup>
+      )}/>
     </Router>
   );
 }
